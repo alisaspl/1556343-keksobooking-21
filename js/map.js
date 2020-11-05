76b711c136abedaa2e1d1ccbdb5c28b2942bef00
unknown <alisaspl@mail.ru>
2020-11-05 18:32:30 +0300

    moving main pin

--- js/map.js
+++ js/map.js
@@ -8,6 +8,7 @@
     map: document.querySelector(`.map`),
     mapPinMain: document.querySelector(`.map__pin--main`),
     mapPins: document.querySelector(`.map__pins`),
+    htmlTag: document.getElementsByTagName(`html`)[0],
   };
 
   let mapPinTemplate = document.querySelector(`#pin`);
@@ -17,7 +18,47 @@
     throw new Error(`pin template not found`);
   }
 
+  el.mapPinMain.addEventListener(`mousedown`, onMouseDown);
+
+
   // Functions ////////////////////////////////////////
+
+  function onMouseDown() {
+    document.addEventListener(`mouseup`, onMouseUp);
+    document.addEventListener(`mousemove`, onMouseMove);
+  }
+  function onMouseUp() {
+    document.removeEventListener(`mouseup`, onMouseUp);
+    document.removeEventListener(`mousemove`, onMouseMove);
+  }
+
+  function onMouseMove(evt) {
+    const x = evt.pageX - config.mainPin.w / 2 - (el.htmlTag.clientWidth - el.map.clientWidth) / 2;
+    const y = evt.pageY - config.mainPin.h - (el.htmlTag.clientHeight - el.map.clientHeight) / 2;
+
+    const minX = config.map.minX - config.mainPin.w / 2;
+    const maxX = el.map.clientWidth - config.mainPin.w / 2;
+
+    if (x > minX && x < maxX) {
+      el.mapPinMain.style.left = x + `px`;
+    } else {
+      if (x < minX) {
+        el.mapPinMain.style.left = minX + `px`;
+      } else {
+        el.mapPinMain.style.left = maxX + `px`;
+      }
+    }
+
+    if (y > config.map.minY && y < config.map.maxY) {
+      el.mapPinMain.style.top = y + `px`;
+    } else {
+      if (y < config.map.maxY) {
+        el.mapPinMain.style.top = config.map.minY + `px`;
+      } else {
+        el.mapPinMain.style.top = config.map.maxY + `px`;
+      }
+    }
+  }
 
   function renderPinsOnMap(advsData) {
     el.mapPins.querySelectorAll(`button.map__pin`).forEach((element) => {
